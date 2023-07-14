f964729d794caea60e23e3b560ac05b0db1d2a09
gev_org <thecarguyarm@gmail.com>
2023-07-14 13:26:41 +0400

    added env file

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,10 @@
 import { createConnection } from 'mysql2/promise';
+require('dotenv').config();
 
 export const db = createConnection({
-  host: 'localhost',
-  user: 'username',
-  password: 'password',
+  host: 'DB_HOST',
+  user: 'DB_USER',
+  password: 'DB_PASSWORD',
   database: 'Homework',
   port: 3306,
 });
